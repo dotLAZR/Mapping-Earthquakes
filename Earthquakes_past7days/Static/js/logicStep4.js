bb97cec27f4831a2642e7a840b7f1e0aac6810e2
luis-zavala <you@example.com>
2022-09-14 21:12:25 -0400

    options

--- Earthquakes_past7days/Static/js/logicStep4.js
+++ Earthquakes_past7days/Static/js/logicStep4.js
@@ -20,15 +20,12 @@ let baseMaps = {
 
 // Create the earthquake layer for our map.
 let earthquakes = new L.layerGroup();
+
 // We define an object that contains the overlays.
 // This overlay will be visible all the time.
 let overlays = {
   Earthquakes: earthquakes
 };
-
-// Then we add a control to the map that will allow the user to change
-// which layers are visible.
-L.control.layers(baseMaps, overlays).addTo(map);
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
@@ -37,7 +34,7 @@ let map = L.map('mapid', {
   layers: [streets]
 })
 // Pass our map layers into our layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
+L.control.layers(baseMaps, overlays).addTo(map);
 
 // Then we add our 'graymap' tile layer to the map.
 // streets.addTo(map);
@@ -90,19 +87,22 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     }
     return magnitude * 4;
   }
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data, {
-    // We turn each feature into a circleMarker on the map.
-    pointToLayer: function (feature, latlng) {
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data, {
+  // We turn each feature into a circleMarker on the map.
+  pointToLayer: function(feature, latlng) {
       console.log(data);
       return L.circleMarker(latlng);
     },
-    // We set the style for each circleMarker using our styleInfo function.
-    style: styleInfo,
-    // We create a popup for each circleMarker to display the magnitude and
-    //  location of the earthquake after the marker has been created and styled.
-    onEachFeature: function (feature, layer) {
-      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
-    }
-  }).addTo(map);
+  // We set the style for each circleMarker using our styleInfo function.
+style: styleInfo,
+  // We create a popup for each circleMarker to display the magnitude and
+  //  location of the earthquake after the marker has been created and styled.
+  onEachFeature: function(feature, layer) {
+  layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+}
+}).addTo(earthquakes);
+
+ // then we add the earthquake layer
+ earthquakes.addTo(map);
 });
